1e7ffb7e6266f9d6ad226d31e906daec7beac80f
Arthur Fontaine <0arthur.fontaine@gmail.com>
2024-10-18 14:38:47 +0200

    Add sitemap Astro plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: false })],
+  site: "https://www.ziboulette.org",
+  integrations: [tailwind({ applyBaseStyles: false }), sitemap()],
   i18n: {
     defaultLocale: "en",
     locales: ["en", "fr"],
